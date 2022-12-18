c93fad7e781ea7cb2f9da8ad94498ca5b65fc750
JoHyunwoo <jhw3874@gmail.com>
2022-12-18 14:13:01 +0900

    스프링 컨테이너 생성

--- demo 2/src/main/java/com/example/demo/AppConfig.java
+++ demo 2/src/main/java/com/example/demo/AppConfig.java
@@ -9,23 +9,27 @@ import com.example.demo.member.MemberServiceImpl;
 import com.example.demo.member.MemoryMemberRepository;
 import com.example.demo.order.OrderService;
 import com.example.demo.order.OrderServiceImpl;
-// AppConfig는 공연 기획자!
-public class AppConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+// AppConfig는 공연 기획자!
+@Configuration
+public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
